5748ecfab43d9198e191c0b1547ff32ab3f8e38c
Judicaël <miharii@proton.me>
2025-06-21 13:39:09 +0300

    define some macros

--- wheel.h
+++ wheel.h
@@ -3,11 +3,11 @@
 
 #include <X11/Xlib.h>
 
-#define GREEN 0x00FF00
-#define RED 0xFF0000
-#define BLACK 0x000000
-#define WHITE 0xFFFFFF
 
 void draw_roulette_wheel(Display *dpy, Window win, GC gc, int width, int height);
 
 #endif
+#define GREEN 0x00FF00
+#define RED 0xFF0000
+#define BLACK 0x000000
+#define WHITE 0xFFFFFF
